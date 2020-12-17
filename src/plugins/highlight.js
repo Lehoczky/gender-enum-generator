05a36091db3b529f95b1fedd4d3165b71d9fa81d
Lehoczky <ifjlehoczkyzoltan@gmail.com>
2020-12-18 00:47:47 +0100

    optimize highlightjs imports

--- src/plugins/highlight.js
+++ src/plugins/highlight.js
@@ -1,5 +1,10 @@
 import Vue from "vue";
-import * as hljs from "highlight.js";
-import _styles from "highlight.js/styles/default.css";
+import hljs from "highlight.js/lib/core";
+import _style from "highlight.js/styles/default.css";
 
+import typescript from "highlight.js/lib/languages/typescript";
+import python from "highlight.js/lib/languages/python";
+
+hljs.registerLanguage("typescript", typescript);
+hljs.registerLanguage("python", python);
 Vue.use(hljs.vuePlugin);
